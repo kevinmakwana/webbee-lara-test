15ff2af8866c3838e5e84ca94e81820dcc8853cf
Kevin Makwana <kevinmakwana@Kevins-MacBook-Air.local>
2022-11-08 11:13:31 -0800

    Schema for online movie show booking created

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -36,7 +36,111 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+
+        Schema::create('cinema_owners', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('cinemas', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('total_cinema_halls');
+            $table->integer('cinema_owner_id')->unsigned();
+            $table->foreign('cinema_owner_id')->references('id')->on('cinema_owners')->onDelete('cascade');
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_halls', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('total_seats');
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_hall_seats', function($table) {
+            $table->increments('id');
+            $table->integer('seat_number')->index();
+            $table->enum('type',['vip_seat','couple_seat','super_vip']);
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->mediumText('description');
+            $table->dateTime('release_date');
+            $table->string('language')->index();
+            $table->string('genre')->index();
+            $table->string('country');
+            $table->timestamps();
+        });
+
+        Schema::create('shows', function($table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->integer('cinema_hall_id')->unsigned();
+            $table->foreign('cinema_hall_id')->references('id')->on('cinema_halls')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function($table) {
+            $table->increments('id');
+            $table->integer('number_of_seats');
+
+            $table->enum('status',['pending','processing','confirmed','failed']);
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+
+        Schema::create('show_seats', function($table) {
+            $table->increments('id');
+
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            
+            $table->integer('cinema_hall_seat_id')->unsigned();
+            $table->foreign('cinema_hall_seat_id')->references('id')->on('cinema_hall_seats')->onDelete('cascade');
+            
+            $table->decimal('price', 8, 2);
+
+            $table->enum('status',['pending','confirmed']);
+            
+            $table->timestamps();
+        });
+
+        Schema::create('booking_payments', function($table) {
+            $table->increments('id');
+
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+
+            $table->decimal('amount', 8, 2);
+
+            $table->string('payment_method');
+
+            $table->timestamps();
+        });
+
+        
     }
 
     /**
